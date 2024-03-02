d69ca4429f13c9642e85ca4f2fa2d2c33e6f15a2
Brenno <brennofruhauf@outlook.com>
2024-03-02 18:20:30 -0300

    feat: add active menu item provider

---
+++ src/hooks/useActiveMenuItem.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
+
+import { useLocale } from 'next-intl'
+
+const ActiveMenuItemContext = createContext('')
+
+export const useActiveMenuItem = () => useContext(ActiveMenuItemContext)
+
+type Props = {
+  children: ReactNode
+}
+
+export function ActiveMenuItemProvider({ children }: Props) {
+  const locale = useLocale()
+
+  const initialState = locale === 'pt-BR' ? 'inicio' : 'home'
+  const [activeMenuItem, setActiveMenuItem] = useState(initialState)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section')
+      const scrollPosition = window.scrollY
+
+      sections.forEach((section) => {
+        const top = section.offsetTop
+        const height = section.offsetHeight
+
+        if (scrollPosition >= top && scrollPosition < top + height)
+          setActiveMenuItem(section.id)
+      })
+    }
+
+    handleScroll()
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <ActiveMenuItemContext.Provider value={activeMenuItem}>
+      {children}
+    </ActiveMenuItemContext.Provider>
+  )
+}
