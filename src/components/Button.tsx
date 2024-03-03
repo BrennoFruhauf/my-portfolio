58eeff81bd32a8fb8978f6c84b348d6de6bfb5be
Brenno <brennofruhauf@outlook.com>
2024-03-03 18:45:57 -0300

    refactor: fix link prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,17 @@
-import Link from 'next/link'
+import { HTMLAttributeAnchorTarget } from 'react'
+
+import { Link, Pathname } from '@/navigation'
 
 interface Props {
   text: string
-  link: string
+  link: Pathname
+  target?: HTMLAttributeAnchorTarget
 }
 
-export function Button({ text, link }: Props) {
+export function Button({ text, link, target = '_self' }: Props) {
   return (
     <Link
+      target={target}
       href={link}
       className={`inline-block select-none border-2 rounded px-5 py-2 uppercase transition-all duration-500 hover:shadow-primary-500/30 hover:shadow-md relative bg-transparent border-neutral-900 overflow-hidden hover:text-primary-50 hover:border-primary-500 before:content-[''] before:absolute before:top-full before:left-full before:bg-primary-500 before:-z-10 before:h-40 before:w-52 before:rounded-full before:transition-all before:duration-700 hover:before:-top-8 hover:before:-left-8 active:scale-90`}
     >
