0ff1cd2f92ebf7080d239d7d85f4f33c89759db7
Brenno <brennofruhauf@outlook.com>
2024-03-03 18:49:09 -0300

    refactor: change middleware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,17 @@
-import createMiddleware from 'next-intl/middleware'
+import createIntlMiddleware from 'next-intl/middleware'
+import { NextRequest } from 'next/server'
 
 import { i18n } from '@/config/i18n.config'
 
-export default createMiddleware(i18n)
+const intlMiddleware = createIntlMiddleware(i18n)
 
-export const config = {
-  matcher: ['/', `/(pt-BR|en-US)/:path*`]
+export default function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl
+  const regex = new RegExp(`^/(${i18n.locales.join('|')})(/.*)?$`)
+
+  const shouldHandle = pathname === '/' || regex.test(request.nextUrl.pathname)
+
+  if (!shouldHandle) return
+
+  return intlMiddleware(request)
 }
