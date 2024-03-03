54308c460db6046636b299b5a48d9d3f4100ce98
Brenno <brennofruhauf@outlook.com>
2024-03-03 18:42:48 -0300

    feat: add paths & redirect

--- src/config/i18n.config.ts
+++ src/config/i18n.config.ts
@@ -1,14 +1,27 @@
 import { getRequestConfig } from 'next-intl/server'
-import { notFound } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 const defaultLocale = 'en-US'
 export const langs = [defaultLocale, 'pt-BR'] as const
 
 const locales = langs as unknown as string[]
-export const i18n = { defaultLocale, locales, localeDetection: true }
+const pathnames = {
+  '/': '/',
+  '/resume': {
+    'en-US': '/resume',
+    'pt-BR': '/curriculo'
+  }
+}
+
+export const i18n = {
+  defaultLocale,
+  locales,
+  pathnames,
+  localeDetection: true
+}
 
 export default getRequestConfig(async ({ locale }) => {
-  if (!locales.includes(locale)) notFound()
+  if (!locales.includes(locale)) redirect('/')
 
   return {
     messages: await import(`../dictionary/${locale}.json`)
